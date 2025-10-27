258283e31216c1f8dd9192aed7376b240efdd5c2
Christian <christiandennis67@gmail.com>
2025-10-26 23:47:38 -0400

    Build simple user interface, add API fetch and display function

---
+++ script.js
@@ -0,0 +1,36 @@
+// fetch method with .then/.catch construction
+
+// fetch("https://pokeapi.co/api/v2/pokemon/lucario")
+//     .then(response => {
+//         if(!response.ok) {
+//             throw new Error("Could not fetch resource");
+//         }
+//         return response.json();
+//     })
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+
+// fetch method with async/await construction
+
+async function fetchData() {
+    try{
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok) {
+            throw new Error("Could not fetch resource");
+        }
+        
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    } 
+    catch(error){
+        console.error(error);
+    }
+}
+
